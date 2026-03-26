d76899607782439841337a8a6f2e94adb50e894d
Muttha Navya Sri <mutthasailaja@gmail.com>
2026-03-26 23:51:12 +0530

    fix: Update ALLOWED_HOSTS to hotel-management-api.onrender.com

--- backend/hotelmanagement/settings.py
+++ backend/hotelmanagement/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-d-e2dh!rqnz06a*wl(fxg=74y!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,hotelmanagementsystem-hotelhive.onrender.com,.onrender.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,hotel-management-api.onrender.com').split(',')
 ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS]
 
 
